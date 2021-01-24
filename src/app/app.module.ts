1d9de79a5c46b896ee38402998c036353487f87e
Avinash <avinash.1920.singh@gmail.com>
2021-01-24 23:19:48 +0530

    commit after filter addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 import { FilterComponent } from './filter/filter.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,HomeComponent,
-    FilterComponent
+    FilterComponent,CardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -6,5 +6,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
-  
+  staticYearFilter=[
+   
+  ]
+  staticFilter=[
+    {
+      'filterText':'Launch Year',
+      'data':[
+        {
+          filterName:'launchYear',
+          value:2012
+        },
+        {
+          filterName:'launchYear',
+          value:2013
+        },
+        {
+          filterName:'launchYear',
+          value:2014
+        },
+        {
+          filterName:'launchYear',
+          value:2015
+        },
+        {
+          filterName:'launchYear',
+          value:2016
+        },
+        {
+          filterName:'launchYear',
+          value:2017
+        },
+        {
+          filterName:'launchYear',
+          value:2018
+        },
+        {
+          filterName:'launchYear',
+          value:2019
+        },
+        {
+          filterName:'launchYear',
+          value:2020
+        }    
+      ]
+    },
+    {
+      'filterText':'Successful Launch',
+      'data':[
+        {
+          filterName:'launch_success',
+          value:'True'
+        },
+        {
+          filterName:'launch_success',
+          value:'False'
+        }
+      ]
+    },
+    {
+      'filterText':'Successful Landing',
+      'data':[
+        {
+          filterName:'land_success',
+          value:'True'
+        },
+        {
+          filterName:'land_success',
+          value:'False'
+        }
+      ]
+    }
+  ]
 }

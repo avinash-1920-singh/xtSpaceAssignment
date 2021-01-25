bed1498b219b34c6dd6093026e23b30c4ac1e015
Avinash Singh <avinash.1920.singh@gmail.com>
2021-01-25 11:46:05 +0530

    some changes

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,12 +3,13 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [HomeComponent],
 })
 export class AppServerModule {}
